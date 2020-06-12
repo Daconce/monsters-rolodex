8171ccfad1fa992db1a08e1f4887511816e5525c
Daconce <luisdaconceicao@gmail.com>
2020-06-12 02:03:45 -0300

    Updates

---
+++ precache-manifest.dd7188faaf866ddbbe2338aec871a522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc82a8fad090e37ec123960d9b94d21",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cab08b855d6636fa6d6f",
+    "url": "/monsters-rolodex/static/css/main.4247cf5a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cab08b855d6636fa6d6f",
+    "url": "/monsters-rolodex/static/js/main.664898bf.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
